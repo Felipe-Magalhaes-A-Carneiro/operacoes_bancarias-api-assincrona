32f4e9e884071017611dd5678d27ee55a7a39a85
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-05-20 12:26:09 -0300

    feat: cria database

---
+++ src/app/database.py
@@ -0,0 +1,8 @@
+import sqlalchemy
+import databases
+
+DATABASE_URL = "sqlite:///./bank.db"
+
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+engine  = sqlalchemy.create_engine(DATABASE_URL, connect_args={"check"})
